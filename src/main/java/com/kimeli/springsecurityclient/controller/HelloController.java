daad379dc6d546d171d0821bf944eec86ce475ee
Kimeli <jobkimeli2016@gmail.com>
2022-11-03 08:19:34 +0300

    Added configurations for authentication on application.yml and websecurity to ensure all apis coming to be authenticated

--- src/main/java/com/kimeli/springsecurityclient/controller/HelloController.java
+++ src/main/java/com/kimeli/springsecurityclient/controller/HelloController.java
@@ -3,10 +3,12 @@ package com.kimeli.springsecurityclient.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class HelloController {
     @GetMapping("/api/hello")
-    public String hellowThereKimeli(){
-        return "Hello, Welcome to SpringBoot Project 18";
+    public String hellowThereKimeli(Principal principal){
+        return "Hello,"+principal.getName()+" Welcome to SpringBoot Project 18";
     }
 }
